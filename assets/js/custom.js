adc9bd180f3c969e3d8d0c9ac5bef7850399b06c
Sumaiya-904 <sumaiyabee211@gmail.com>
2024-09-14 13:07:08 +0530

    bugs fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,44 +1,23 @@
-// Hero Image Carousel
-let currentIndex = 0;
-let slides = document.querySelectorAll(".carousel-slide img");
-let dots = document.querySelectorAll(".dot");
-let totalSlides = slides.length;
-let autoSlideInterval;
-
-function showSlide(index) {
-  const slideWidth = document.querySelector(".carousel-container").clientWidth;
-  document.querySelector(".carousel-slide").style.transform = `translateX(-${
-    index * slideWidth
-  }px)`;
-
-  dots.forEach((dot) => dot.classList.remove("active"));
-  dots[index].classList.add("active");
-}
-
-function currentSlide(index) {
-  currentIndex = index;
-  showSlide(index);
-}
-
-function autoSlide() {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  showSlide(currentIndex);
-}
-
-function startAutoSlide() {
-  autoSlideInterval = setInterval(autoSlide, 5000);
-}
-
-function stopAutoSlide() {
-  clearInterval(autoSlideInterval);
-}
-
-startAutoSlide();
-
-slides.forEach((slide) => {
-  slide.addEventListener("mouseover", stopAutoSlide);
-
-  slide.addEventListener("mouseout", startAutoSlide);
-
-  slide.addEventListener("click", stopAutoSlide);
+$(document).ready(function () {
+  $(".carousel_slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+    navText: ["&#10094;", "&#10095;"],
+  });
 });
